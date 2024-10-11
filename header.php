c9b85c6eecf5d12d54343a4821f43e43edc59e1c
olsiasllani <olsiasllani08@gmail.com>
2024-10-11 10:48:44 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+
+  <link href="
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css
+" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+
+  <div class="container">
+  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+      <img src="Logo_white_DigitalSchool.png" width="100px">
+    </a>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+      <li><a href="index.php" class="nav-link px-2 link-secondary">Home</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
+      <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+      <button type="button" class="btn btn-outline-primary me-2" onclick="window.location.href='login.php';">Login</button>
+      <button type="button" class="btn btn-primary" onclick="window.location.href='signup.php';">Sign-up</button>
+    </div>
+  </header>
+  </div>
+
+  <main class="min-vh-100 bg-light d-flex justify-content-center"> 
--- login.php
+++ login.php
@@ -1,9 +1,6 @@
 
-
 <?php include_once 'header.php'; ?>
     
   <h1>Bootstrap</h1>
 
 <?php include_once 'footer.php'; ?> 
-
-
